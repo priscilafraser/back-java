6140530d035df67ffa7e06e2c0236344806de92a
Priscila Miranda <priscila.fraser@gmail.com>
2022-09-13 13:17:23 -0300

    teste4

--- src/main/java/com/pet/controller/ProdutoController.java
+++ src/main/java/com/pet/controller/ProdutoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pet.entidades.Categoria;
 import com.pet.entidades.Produto;
-import com.pet.repository.ProdutoRepository;
-import com.pet.services.CategoriaService;
 import com.pet.services.ProdutoService;
-import com.pet.services.dto.CategoriaDTO;
 import com.pet.services.dto.ProdutoDTO;
 
 
--- src/main/java/com/pet/entidades/Fornecedor.java
+++ src/main/java/com/pet/entidades/Fornecedor.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 
 @Entity
 public class Fornecedor {
