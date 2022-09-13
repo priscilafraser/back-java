94f760f1f7f4dfd37d690191f066d7aeb2918a7e
Priscila Miranda (fraser) <55546267+priscilafraser@users.noreply.github.com>
2022-09-13 13:35:36 -0300

    Update LigacaoFornecedorProdutoRepository.java

--- src/main/java/com/pet/repository/LigacaoFornecedorProdutoRepository.java
+++ src/main/java/com/pet/repository/LigacaoFornecedorProdutoRepository.java
@@ -13,9 +13,6 @@ import com.pet.entidades.LigacaoFornecedorProduto;
 public interface LigacaoFornecedorProdutoRepository extends JpaRepository<LigacaoFornecedorProduto, Long> {
 	
 	
-//	@Query(value="select descricao, imagem, preco from Ligacao_Fornecedor_Produto where idfornecedor= :idfornecedor", nativeQuery=true)
-//	List<LigacaoFornecedorProduto> getProdutosPorFornecedor(@Param("idfornecedor") Long idfornecedor);
-	
 	@Query("select lp from LigacaoFornecedorProduto lp where lp.fornecedor.id= :idfornecedor")
 	List<LigacaoFornecedorProduto> getProdutosPorFornecedor(@Param("idfornecedor") Long idfornecedor);
 
